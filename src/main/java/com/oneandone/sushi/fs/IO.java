fb0d001b5f97b7a869a65e1b7bae87a01fc77e8c
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2011-02-03 22:06:06 +0000

    rename

--- src/main/java/com/oneandone/sushi/fs/IO.java
+++ src/main/java/com/oneandone/sushi/fs/IO.java
@@ -55,7 +55,7 @@ import com.oneandone.sushi.xml.Xml;
  * <p>You can creates as many IO objects as you which, but using nodes from different IO objectes cannot interact
  * so you'll usually stick with a single IO instance.</p>
  *
- * <p>TODO: Multi-Threading. Currently, you need to know fs system internals to propertly synchronized
+ * <p>TODO: Multi-Threading. Currently, you need to know fs system internals to properly synchronized
  * multi-threaded applications.</p>
  */
 public class IO {

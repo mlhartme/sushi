dcf89591b5ef07ab3ad6ef9250479003dd6c8c33
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2011-06-30 08:08:52 +0000

    license check and fixes

--- src/main/java/net/sf/beezle/sushi/fs/Features.java
+++ src/main/java/net/sf/beezle/sushi/fs/Features.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 1&1 Internet AG, http://www.1and1.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.sf.beezle.sushi.fs;
 
 /**
@@ -12,7 +29,7 @@ public class Features {
 
     /** if not, move is emulated by copy and delete */
     public final boolean nativeMove;
-    
+
     /** if not, link methods throw an UnsupportedOperationException */
     public final boolean links;
 
--- src/main/java/net/sf/beezle/sushi/fs/webdav/LineLogger.java
+++ src/main/java/net/sf/beezle/sushi/fs/webdav/LineLogger.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 1&1 Internet AG, http://www.1and1.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.sf.beezle.sushi.fs.webdav;
 
 import net.sf.beezle.sushi.util.Strings;
@@ -19,7 +36,7 @@ public class LineLogger {
     public void log(byte ... bytes) {
     	log(bytes, 0, bytes.length);
     }
-    
+
     public void log(byte[] bytes, int ofs, int length) {
     	log(new String(bytes, ofs, length));
     }

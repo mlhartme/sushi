b86a88a97517fc539acd1d8ef411ea71ea6f78cc
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2011-09-16 11:33:37 +0000

    mkfile exception message

--- src/main/java/net/sf/beezle/sushi/fs/MkfileException.java
+++ src/main/java/net/sf/beezle/sushi/fs/MkfileException.java
@@ -19,9 +19,9 @@ package net.sf.beezle.sushi.fs;
 
 public class MkfileException extends NodeException {
     public MkfileException(Node node) {
-        super(node, "mkdir failed");
+        super(node, "mkfile failed");
     }
-    
+
     public MkfileException(Node node, Throwable e) {
         this(node);
         initCause(e);

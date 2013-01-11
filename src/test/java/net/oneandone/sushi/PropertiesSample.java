73e1c586864fce40b110eb774064dc5528210ed0
Michael Hartmeier <mlhartme@github.com>
2013-01-11 20:19:39 +0100

    improved properties example

--- src/test/java/net/oneandone/sushi/PropertiesSample.java
+++ src/test/java/net/oneandone/sushi/PropertiesSample.java
@@ -15,45 +15,62 @@
  */
 package net.oneandone.sushi;
 
+import net.oneandone.sushi.fs.Node;
 import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.metadata.Instance;
 import net.oneandone.sushi.metadata.Type;
 import net.oneandone.sushi.metadata.reflect.ReflectSchema;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesSample {
+    private static final World world = new World();
+
     /** Serialize object to xml and load the result back into an object */
-    public static void main(String[] args) {
-        Properties props;
-        Instance<Obj> data;
-        Obj obj;
-        
-        props = new Properties();
-        props.setProperty("foo.number", "2");
-        props.setProperty("foo.string", "hi");
-        data = TYPE.loadProperties(props, "foo");
-        obj = data.get();
-        System.out.println("object:\n" + obj);
-        obj.number = 3;
-        System.out.println("properties:\n" + data.toProperties("bar"));
+    public static void main(String[] args) throws IOException {
+        Node propfile;
+        Config config;
+
+        propfile = world.getTemp().createTempFile();
+        config = new Config();
+        config.save(propfile);
+        System.out.println("default config created:");
+        System.out.println(propfile.readString());
+
+        config.number = 2;
+        config.string = "changed";
+        config.save(propfile);
+        System.out.println("saved changed:");
+        System.out.println(propfile.readString());
+
+        config = Config.load(propfile);
+        System.out.println("loaded config: " + config);
     }
     
-    private static final Type TYPE = new ReflectSchema(new World()).type(Obj.class);
-    
-    public static class Obj {
+    public static class Config {
+        private static final Type TYPE = new ReflectSchema(world).type(Config.class);
+
+        public static Config load(Node file) throws IOException {
+            return (Config) TYPE.loadProperties(file.readProperties(), "config").get();
+        }
+
         public int number;
         public String string;
         
-        public Obj() {
+        public Config() {
             this(0, "");
         }
 
-        public Obj(int number, String string) {
+        public Config(int number, String string) {
             this.number = number;
             this.string = string;
         }
-        
+
+        public void save(Node file) throws IOException {
+            file.writeProperties(TYPE.instance(this).toProperties("config"));
+        }
+
         @Override
         public String toString() {
             return "number=" + number + ",string=" + string;

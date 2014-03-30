858864ce077b1fe72a5c4f2e69037ec45d908d68
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-03-30 22:11:57 +0200

    Add initial implementation for NetRcParser.

---
+++ src/main/java/net/oneandone/sushi/util/NetRcParser.java
@@ -0,0 +1,197 @@
+/**
+ * Copyright 1&1 Internet AG, https://github.com/1and1/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.oneandone.sushi.util;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * Basic <tt>.netrc</tt> parser.
+ *
+ * Note that currently neither the <tt>account</tt> nor <tt>macdef</tt> parameters are supported.
+ *
+ * @see <a href="http://hg.python.org/cpython/file/3a1db0d2747e/Lib/netrc.py">Python implementation</a>
+ * @see <a href="http://linux.die.net/man/5/netrc">netrc man page</a>
+ *
+ * @author Mirko Friedenhagen
+ */
+public class NetRcParser {
+
+    private final static CharSequence ADDITIONAL_CHARS = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+    final HashMap<String, NetRcAuthenticator> authenticators = new HashMap<>();
+
+    /**
+     * Returns a {@link NetRcAuthenticator} for the given host.
+     *
+     * @param hostname to look for.
+     * @return authentication information for hostname.
+     */
+    public NetRcAuthenticator getAuthenticators(final String hostname) {
+        return authenticators.get(hostname);
+    }
+
+    public void parse(InputStreamReader in) {
+        final NetRcStreamTokenizer tokenizer = new NetRcStreamTokenizer(in);
+        try {
+            String entryName = null;
+            String login;
+            String password;
+            String toplevel;
+            while (true) {
+                int tt = tokenizer.nextToken();
+                final String sval = tokenizer.sval;
+                toplevel = sval;
+                if (tt == StreamTokenizer.TT_EOF) {
+                    break;
+                } else if (sval.equals("machine")) {
+                    tokenizer.nextToken();
+                    entryName = tokenizer.sval;
+                } else if (sval.equals("default")) {
+                    entryName = "default";
+                } else if (sval.equals("macdef")) {
+                    throw new NetRcIllegalArgumentException(("macdef not supported"), tokenizer.lineno(), sval);
+                } else {
+                    throw new NetRcIllegalArgumentException("bad toplevel", tokenizer.lineno(), toplevel);
+                }
+                login = "";
+                password = null;
+                while (true) {
+                    tokenizer.nextToken();
+                    final String sval1 = tokenizer.sval;
+                    if (sval1 == null || sval1.startsWith("#") || sval1.equals("machine") || sval1.equals("") || sval1.equals("default")) {
+                        if (password != null) {
+                            authenticators.put(entryName, new NetRcAuthenticator(login, password));
+                            tokenizer.pushBack();
+                            break;
+                        } else {
+                            throw new NetRcIllegalArgumentException("malformed token at toplevel " + toplevel, tokenizer.lineno(), sval1);
+                        }
+                    } else if (sval1.equals("login") || sval1.equals("user")) {
+                        tokenizer.nextToken();
+                        login = tokenizer.sval;
+                    } else if (sval1.equals("account")) {
+                        throw new NetRcIllegalArgumentException("account not supported", tokenizer.lineno(), sval1);
+                    } else if (sval1.equals("password")) {
+                        tokenizer.nextToken();
+                        password = tokenizer.sval;
+                    } else {
+                        throw new NetRcIllegalArgumentException("bad follower token", tokenizer.lineno(), sval1);
+                    }
+                }
+            }
+
+        }
+        catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+
+    /**
+     * Holder class for authentication information.
+     */
+    public static class NetRcAuthenticator {
+
+        private final String user;
+        private final String pass;
+
+        /**
+         * @param user username
+         * @param pass password
+         */
+        public NetRcAuthenticator(String user, String pass) {
+            this.user = user;
+            this.pass = pass;
+        }
+
+        /**
+         * @return the user
+         */
+        public String getUser() {
+            return user;
+        }
+
+        /**
+         * @return the pass
+         */
+        public String getPass() {
+            return pass;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 5;
+            hash = 17 * hash + Objects.hashCode(this.user);
+            hash = 17 * hash + Objects.hashCode(this.pass);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final NetRcAuthenticator other = (NetRcAuthenticator) obj;
+            if (!Objects.equals(this.user, other.user)) {
+                return false;
+            }
+            if (!Objects.equals(this.pass, other.pass)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "NetRcAuthenticator{" + "user=" + user + ", pass=" + pass + '}';
+        }
+
+    }
+
+    public static class NetRcIllegalArgumentException extends IllegalArgumentException {
+        public NetRcIllegalArgumentException(String message, int lineNo, String token) {
+            super(message + " at line " + lineNo + ": " + token);
+        }
+    }
+
+    private class NetRcStreamTokenizer extends StreamTokenizer {
+
+        public NetRcStreamTokenizer(Reader r) {
+            super(r);
+        }
+        {
+            resetSyntax();
+            wordChars('a', 'z');
+            wordChars('A', 'Z');
+            wordChars('0', '9');
+            wordChars(128 + 32, 255);
+            whitespaceChars(0, ' ');
+            for (int i = 0; i < ADDITIONAL_CHARS.length(); i++) {
+                wordChars(ADDITIONAL_CHARS.charAt(i), ADDITIONAL_CHARS.charAt(i));
+            }
+            commentChar('#');
+        }
+    }
+
+}
---
+++ src/test/java/net/oneandone/sushi/util/NetRcParserTest.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright 1&1 Internet AG, https://github.com/1and1/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.oneandone.sushi.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ *
+ * @author Mirko Friedenhagen
+ */
+public class NetRcParserTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    NetRcParser sut;
+
+    @Before
+    public void setUp() {
+        sut = new NetRcParser();
+    }
+
+    @Test
+    public void testParse() {
+        InputStreamReader in = createInputStreamReader(
+                "machine svn.dev.java.net login user password pass\n"+
+                "machine svn2.dev.java.net user user2 password päss2\n" +
+                "#machine svn3.dev.java.net user user3 password pass3\n" +
+                "default login userd password passd");
+        sut.parse(in);
+        Assert.assertEquals(new NetRcParser.NetRcAuthenticator("user", "pass"), sut.getAuthenticators("svn.dev.java.net"));
+        Assert.assertEquals(new NetRcParser.NetRcAuthenticator("user2", "päss2"), sut.getAuthenticators("svn2.dev.java.net"));
+        Assert.assertNull(sut.getAuthenticators("svn3.dev.java.net"));
+        final NetRcParser.NetRcAuthenticator defaultAuthenticator = sut.getAuthenticators("default");
+        Assert.assertEquals(new NetRcParser.NetRcAuthenticator("userd", "passd"), defaultAuthenticator);
+        Assert.assertEquals("userd", defaultAuthenticator.getUser());
+        Assert.assertEquals("passd", defaultAuthenticator.getPass());
+        Assert.assertEquals("NetRcAuthenticator{user=userd, pass=passd}", String.valueOf(defaultAuthenticator));
+    }
+
+    @Test
+    public void testParseMacDefNotSupported() {
+        InputStreamReader in = createInputStreamReader(
+                "macdef foo");
+        expectedException.expect(NetRcParser.NetRcIllegalArgumentException.class);
+        expectedException.expectMessage("macdef not supported at line 1: macdef");
+        sut.parse(in);
+    }
+
+    @Test
+    public void testParseAccountNotSupported() {
+        InputStreamReader in = createInputStreamReader(
+                "machine svn.dev.java.net login user account account\n");
+        expectedException.expect(NetRcParser.NetRcIllegalArgumentException.class);
+        expectedException.expectMessage("account not supported at line 1: account");
+        sut.parse(in);
+    }
+
+    @Test
+    public void testParseBadTopLevel() {
+        InputStreamReader in = createInputStreamReader(
+                "fizzbuzz svn.dev.java.net login user pass account\n");
+        expectedException.expect(NetRcParser.NetRcIllegalArgumentException.class);
+        expectedException.expectMessage("bad toplevel at line 1: fizzbuzz");
+        sut.parse(in);
+    }
+
+    @Test
+    public void testParseBadEntryWithBadFollowerToken() {
+        InputStreamReader in = createInputStreamReader(
+                "machine svn.dev.java.net login user puss account\n");
+        expectedException.expect(NetRcParser.NetRcIllegalArgumentException.class);
+        expectedException.expectMessage("bad follower token at line 1: puss");
+        sut.parse(in);
+    }
+
+    @Test
+    public void testParseBadEntryWithoutPassword() {
+        InputStreamReader in = createInputStreamReader(
+                "machine svn.dev.java.net login user\n");
+        expectedException.expect(NetRcParser.NetRcIllegalArgumentException.class);
+        expectedException.expectMessage("malformed token at toplevel machine at line 2: null");
+        sut.parse(in);
+    }
+
+    private InputStreamReader createInputStreamReader(final String netrc) {
+        final ByteArrayInputStream inBytes = new ByteArrayInputStream(
+                netrc.getBytes(Charset.forName("utf-8")));
+        InputStreamReader in = new InputStreamReader(inBytes);
+        return in;
+    }
+
+}
